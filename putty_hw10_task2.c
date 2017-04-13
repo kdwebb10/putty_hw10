6961e03bcd6c3486668655b4a2ce91651993f3aa
kyle webb <kylewebb4@weber.edu>
2017-04-13 15:49:55 +0000

    Created Vim

---
+++ putty_hw10_task2.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  putty_hw10_task2.c
+ *       	Usage:  ./putty_hw10_task2.c
+ *    Description:  task2
+ *
+ *        Version:  1.0
+ *        Created:  04/13/2017 03:47:28 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Kyle Webb (), kylewebbear@weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>		/* Standard Library */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
