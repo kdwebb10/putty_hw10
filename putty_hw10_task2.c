51c5133a1e57a8e5e8d7d029bbca58198da77495
braxtongreenwell <braxtongreenwell@mail.weber.edu>
2017-04-15 22:05:50 +0000

    adding .gitignore and usage function for task2

--- putty_hw10_task2.c
+++ putty_hw10_task2.c
@@ -13,17 +13,29 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>		/* Standard Library */
+#include <string.h>
 
 /* Function Prototypes */
-
+void Usage(char** info);
 /* Main Program */
 int main(int argc, char *argv[])
 {
+	if(argc != 2 || strcmp((argv[1]), "--help")==0)
+	{
+		Usage(argv);
+	}
 
 	return 0;
 }
 
 
 /* Function Defenitions */
+void Usage(char** info)
+{
+	printf("Error opening data file.\n");
+	printf("Usage: ./<file name>\n");
+	exit(1);
+	return;
+}
 
 
