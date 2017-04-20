f68cdaa70032b77affde17a56594aac576c39ff0
braxtongreenwell <braxtongreenwell@mail.weber.edu>
2017-04-19 20:03:12 -0600

    final commit task2

--- putty_hw10_task2.c
+++ putty_hw10_task2.c
@@ -56,11 +56,11 @@ void ReadFile(char *file1, float data[])
 {
 	FILE *infile;
 	int i;
-	int x = 1;
+	int x = 10;
 	double height = 0;
-	double all = 0;
+	double max = 0;
 	double avg;
-	struct tsunami height[SIZE];
+	struct tsunami surf[SIZE];
 	infile = fopen(file1, "r");
 	if(infile == NULL)
 	{
@@ -68,24 +68,23 @@ void ReadFile(char *file1, float data[])
 		exit(1);
 	}
 		printf("The txt file %s was successfully read. \n", file1);
-	while(fscanf(infile, "%d %d %d %d %lf %s", &surf[i].mo, &surf[i].da, &surf[i].yr, &surf[i].fatalities, &surf[i].max_height, &surf[i].location) != EOF)
+	while(fscanf(infile, "%d %d %d %d %lf %s", &surf[i].mo, &surf[i].da, &surf[i].yr, &surf[i].fatalities, &surf[i].max_height, surf[i].location) != EOF)
 	{
-		all = all + surf[i].max_height;
+		max = max + surf[i].max_height;
 		if(surf[i].max_height > height)
 		{
 			height = surf[i].max_height;
 			i++;
 		}	
 	}
-	x = 1;
-	avg = all/x;
-	printf("Max height in feet is: %.2f\n", height*3.28);	//why *3.28?
-	printf("The average height in feet is: %.2f\n", average*3.28);
+	avg = max/x;
+	printf("Max height in feet is: %.2f\n", height);	//why *3.28?
+	printf("The average height in feet is: %.2f\n", avg);
 	printf("List of tsunamis with a height greater than average: \n");
 	
-	for(i = 0; i < j; i++)
+	for(i = 0; i < x; i++)
 	{
-		if(wave[i].maxheight > avg)
+		if(surf[i].max_height > avg)
 		{
 			printf("%s \n", surf[i].location);
 		}
@@ -93,5 +92,6 @@ void ReadFile(char *file1, float data[])
 	fclose(infile);
 	return;
 }
+
 
 
